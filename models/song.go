a9aef7ba314e2d30aba3633585af9b76a1c65c6a
yehaozz <yehao1zhang@gmail.com>
2024-09-08 22:24:16 -0700

    add equality check func for Song type

--- models/song.go
+++ models/song.go
@@ -7,3 +7,10 @@ type Song struct {
 	Artist string   `json:"artist"`
 	Rating *float64 `json:"rating,omitempty"`
 }
+
+func IsSameSong(songOne, songTwo Song) bool {
+	return songOne.ID == songTwo.ID &&
+		songOne.Title == songTwo.Title &&
+		songOne.Artist == songTwo.Artist &&
+		*songOne.Rating == *songTwo.Rating
+}
